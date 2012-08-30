05915671ee0f124a89db3908e148dcc3d8bbeb4b
Michael Vanasse <mike@vancoa.com>
2012-08-29 23:03:11 -0500

    Add image-manager

---
+++ app/modules/image-manager.js
@@ -0,0 +1,33 @@
+
+
+IM = [
+  "baseball",
+  "tomato",
+  "beer_glass",
+  "bottle",
+  "bread",
+  "burger",
+  "cherry",
+  "cupcake",
+  "cup_togo",
+  "dinosaur",
+  "flower",
+  "frozen_yogurt",
+  "grill",
+  "icecream_bar",
+  "icecream_cone",
+  "mug",
+  "mushroom",
+  "pasta",
+  "pie",
+  "pizza",
+  "pizza_slice",
+  "sax",
+  "tomato",
+  "tux",
+  "wine_glass"
+]
+
+
+module.exports = IM;
+
---
+++ test/test.js
@@ -0,0 +1,26 @@
+/* require('../app.js') */
+var Offer = require('../app/modules/account-manager.js')
+
+
+
+
+var should = require('should')
+
+var user = {
+  name: 'User',
+  email: 'user@ex.com',
+  pass: '321321',
+  cust: '111',
+}
+
+describe('Fucking thing', function (){
+  it('should fucking work', function(){
+    Offer.derp(1).should.equal(1)
+  })
+})
+
+describe('Offer', function (){
+  it('should sign up a user', function(){
+  })
+})
+
